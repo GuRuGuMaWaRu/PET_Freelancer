cd917c2b376f9fc15fec1852a192d5b6a9cfe3fa
GuRuGu <peter.k.nets@gmail.com>
2019-09-01 10:06:16 +0300

    Add custom middleware to handle CORS

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  // Set CORS headers so that React SPA is able to communicate with this server
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use("/projects", projectsRouter);
 app.use("/clients", clientsRouter);
 app.use("/", authRouter);
