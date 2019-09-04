c3f4778dd40f71ae526f76ce78bc6f72800433e2
GuRuGu <peter.k.nets@gmail.com>
2019-09-04 11:10:31 +0300

    Update project-save tests to account for new/old client

--- test/project-save.test.js
+++ test/project-save.test.js
@@ -7,9 +7,8 @@ const Client = require("../models/Client");
 describe("Project controller", () => {
   it("should save a new project with a new client on POST request to /api/projects", done => {
     const data = {
-      client: {
-        name: "Client 1"
-      },
+      newClient: "Client 1",
+      client: "Cosmos",
       project: {
         projectNr: "ABC123",
         payment: 1000
@@ -27,6 +26,37 @@ describe("Project controller", () => {
               .then(newCounts => {
                 assert(newCounts[0] === counts[0] + 1);
                 assert(newCounts[1] === counts[1] + 1);
+                done();
+              })
+              .catch(err => {
+                console.log(err);
+              });
+          });
+      }
+    );
+  });
+
+  it("should save a new project with an old client on POST request to /api/projects", done => {
+    const data = {
+      newClient: "",
+      client: "Cosmos",
+      project: {
+        projectNr: "ABC123",
+        payment: 1000
+      }
+    };
+
+    Promise.all([Project.countDocuments(), Client.countDocuments()]).then(
+      counts => {
+        request(app)
+          .post("/api/projects")
+          .send(data)
+          .expect(200)
+          .end((err, res) => {
+            Promise.all([Project.countDocuments(), Client.countDocuments()])
+              .then(newCounts => {
+                assert(newCounts[0] === counts[0] + 1);
+                assert(newCounts[1] === counts[1]);
                 done();
               })
               .catch(err => {
