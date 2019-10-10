91ac191989ad7a27fdcd453340e0044a428e9b2e
GuRuGu <peter.k.nets@gmail.com>
2019-10-10 18:37:04 +0300

    Get backend and frontend communicate with one another

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -3,6 +3,7 @@ const Client = require("../models/Client");
 module.exports = {
   index: async (req, res) => {
     const clients = await Client.find().sort({ name: 1 });
+    console.log(clients);
     res.status(200).json(clients);
   }
 };
