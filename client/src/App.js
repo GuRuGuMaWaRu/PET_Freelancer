d3b82960c5de79a3e7b927bc79fa0f5a645355c3
GuRuGu <peter.k.nets@gmail.com>
2019-10-11 11:22:24 +0300

    Install styled-components lib

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { Fragment } from "react";
 import axios from "axios";
+import styled from "styled-components/macro";
 import "./App.css";
+
+const StyledTitle = styled.h1`
+  text-align: center;
+`;
 
 function App() {
   const [projects, setProjects] = React.useState(null);
@@ -13,13 +18,17 @@ function App() {
       })
       .catch(err => console.log(err));
   }, []);
+
   return (
     projects && (
-      <div>
-        {projects.map(project => (
-          <div key={project._id}>{project.projectNr}</div>
-        ))}
-      </div>
+      <Fragment>
+        <StyledTitle>Freelancer</StyledTitle>
+        <div>
+          {projects.map(project => (
+            <div key={project._id}>{project.projectNr}</div>
+          ))}
+        </div>
+      </Fragment>
     )
   );
 }
