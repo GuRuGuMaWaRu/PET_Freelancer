f6a27b451485e13f9a3528ec6e225cd916d201a2
GuRuGu <peter.k.nets@gmail.com>
2019-10-12 13:44:32 +0300

    Use main font-style in all input elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { Fragment } from "react";
 import axios from "axios";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,6 +9,15 @@ import {
 } from "react-router-dom";
 import moment from "moment";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+
+const GlobalStyle = createGlobalStyle`
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit
+  }
+`;
 
 const StyledTitle = styled.h1`
   text-align: center;
@@ -36,133 +45,136 @@ function App() {
   }, []);
 
   return (
-    <Router>
-      <StyledTitle>Freelancer</StyledTitle>
+    <Fragment>
+      <GlobalStyle />
+      <Router>
+        <StyledTitle>Freelancer</StyledTitle>
 
-      <nav>
-        <ul>
-          <li>
-            <NavLink
-              exact
-              to="/"
-              activeStyle={{
-                fontWeight: "bold",
-                color: "red"
-              }}
-            >
-              Projects
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/add"
-              activeStyle={{
-                fontWeight: "bold",
-                color: "red"
-              }}
-            >
-              Add Project
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                exact
+                to="/"
+                activeStyle={{
+                  fontWeight: "bold",
+                  color: "red"
+                }}
+              >
+                Projects
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/add"
+                activeStyle={{
+                  fontWeight: "bold",
+                  color: "red"
+                }}
+              >
+                Add Project
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
 
-      <Switch>
-        <Route
-          path="/add"
-          render={() => (
-            <Formik
-              initialValues={{
-                date: moment().format("YYYY-MM-DD"),
-                client: "",
-                newClient: "",
-                projectNr: "",
-                currency: "USD",
-                payment: 0
-              }}
-              onSubmit={async (values, actions) => {
-                try {
-                  await axios.post("/projects", values);
-                  actions.setSubmitting(false);
-                } catch (err) {
-                  actions.setSubmitting(false);
-                  actions.setStatus({ msg: "Something went wrong" });
-                }
-              }}
-              render={({ errors, status, touched, isSubmitting }) => (
-                <Form>
-                  <div>
-                    <label htmlFor="date">Date:</label>
-                    <Field type="date" name="date" />
-                    <ErrorMessage name="date" component="div" />
-                  </div>
-                  <div>
-                    <label htmlFor="client">Client:</label>
-                    <Field name="client" component="select">
-                      {clients &&
-                        clients.map(client => (
-                          <option key={client._id} value={client._id}>
-                            {client.name}
-                          </option>
-                        ))}
-                    </Field>
-                    <ErrorMessage name="client" component="div" />
-                  </div>
-                  <div>
-                    <label htmlFor="newClient">New client:</label>
-                    <Field type="text" name="newClient" />
-                    <ErrorMessage name="newClient" component="div" />
-                  </div>
-                  <div>
-                    <label htmlFor="projectNr">Project Nr:</label>
-                    <Field type="text" name="projectNr" />
-                    <ErrorMessage name="projectNr" component="div" />
-                  </div>
-                  <div>
-                    <label htmlFor="currency">Currency:</label>
-                    <Field name="currency" component="select">
-                      <option value="USD">USD</option>
-                      <option value="EUR">EUR</option>
-                    </Field>
-                    <ErrorMessage name="currency" component="div" />
-                  </div>
-                  <div>
-                    <label htmlFor="payment">Payment:</label>
-                    <Field type="number" name="payment" />
-                    <ErrorMessage name="payment" component="div" />
-                  </div>
-                  {status && status.msg && <div>{status.msg}</div>}
-                  <button type="submit" disabled={isSubmitting}>
-                    Add
-                  </button>
-                </Form>
-              )}
-            />
-          )}
-        ></Route>
-        <Route
-          exact
-          path="/"
-          render={() =>
-            projects && (
-              <div>
-                {projects.map(project => (
-                  <StyledProject key={project._id}>
-                    Client: {project.client.name}
-                    <br />
-                    Project Nr: {project.projectNr}
-                    <br />
-                    Payment: {project.payment}
-                    <br />
-                    Date: {project.date}
-                  </StyledProject>
-                ))}
-              </div>
-            )
-          }
-        ></Route>
-      </Switch>
-    </Router>
+        <Switch>
+          <Route
+            path="/add"
+            render={() => (
+              <Formik
+                initialValues={{
+                  date: moment().format("YYYY-MM-DD"),
+                  client: "",
+                  newClient: "",
+                  projectNr: "",
+                  currency: "USD",
+                  payment: 0
+                }}
+                onSubmit={async (values, actions) => {
+                  try {
+                    await axios.post("/projects", values);
+                    actions.setSubmitting(false);
+                  } catch (err) {
+                    actions.setSubmitting(false);
+                    actions.setStatus({ msg: "Something went wrong" });
+                  }
+                }}
+                render={({ errors, status, touched, isSubmitting }) => (
+                  <Form>
+                    <div>
+                      <label htmlFor="date">Date:</label>
+                      <Field type="date" name="date" />
+                      <ErrorMessage name="date" component="div" />
+                    </div>
+                    <div>
+                      <label htmlFor="client">Client:</label>
+                      <Field name="client" component="select">
+                        {clients &&
+                          clients.map(client => (
+                            <option key={client._id} value={client._id}>
+                              {client.name}
+                            </option>
+                          ))}
+                      </Field>
+                      <ErrorMessage name="client" component="div" />
+                    </div>
+                    <div>
+                      <label htmlFor="newClient">New client:</label>
+                      <Field type="text" name="newClient" />
+                      <ErrorMessage name="newClient" component="div" />
+                    </div>
+                    <div>
+                      <label htmlFor="projectNr">Project Nr:</label>
+                      <Field type="text" name="projectNr" />
+                      <ErrorMessage name="projectNr" component="div" />
+                    </div>
+                    <div>
+                      <label htmlFor="currency">Currency:</label>
+                      <Field name="currency" component="select">
+                        <option value="USD">USD</option>
+                        <option value="EUR">EUR</option>
+                      </Field>
+                      <ErrorMessage name="currency" component="div" />
+                    </div>
+                    <div>
+                      <label htmlFor="payment">Payment:</label>
+                      <Field type="number" name="payment" />
+                      <ErrorMessage name="payment" component="div" />
+                    </div>
+                    {status && status.msg && <div>{status.msg}</div>}
+                    <button type="submit" disabled={isSubmitting}>
+                      Add
+                    </button>
+                  </Form>
+                )}
+              />
+            )}
+          ></Route>
+          <Route
+            exact
+            path="/"
+            render={() =>
+              projects && (
+                <div>
+                  {projects.map(project => (
+                    <StyledProject key={project._id}>
+                      Client: {project.client.name}
+                      <br />
+                      Project Nr: {project.projectNr}
+                      <br />
+                      Payment: {project.payment}
+                      <br />
+                      Date: {project.date}
+                    </StyledProject>
+                  ))}
+                </div>
+              )
+            }
+          ></Route>
+        </Switch>
+      </Router>
+    </Fragment>
   );
 }
 
