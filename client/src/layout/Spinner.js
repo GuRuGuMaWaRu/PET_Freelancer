a9404fa7c92bc19a9181d924256ad3efc3d8117e
GuRuGu <peter.k.nets@gmail.com>
2019-10-18 15:47:32 +0300

    Replace spinner with the one that has transparent bg

--- client/src/layout/Spinner.js
+++ client/src/layout/Spinner.js
@@ -1,12 +1,14 @@
 import React from "react";
-import spinner from "./spinner.gif";
+import spinner from "./ajax-loader.gif";
+import styled from "styled-components";
 
-const Spinner = () => (
-  <img
-    src={spinner}
-    alt="Loading..."
-    style={{ width: "200px", margin: "auto", display: "block" }}
-  />
-);
+const StyledImg = styled.img`
+  display: block;
+  width: 50px;
+  padding-top: 100px;
+  margin: auto;
+`;
+
+const Spinner = () => <StyledImg src={spinner} alt="Loading..." />;
 
 export default Spinner;
