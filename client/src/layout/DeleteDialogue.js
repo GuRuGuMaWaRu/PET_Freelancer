f2efdda596783c528770f570e3f55bdb770dbfd5
GuRuGu <peter.k.nets@gmail.com>
2019-11-17 08:37:04 +0200

    Style Delete Dialogue

--- client/src/layout/DeleteDialogue.js
+++ client/src/layout/DeleteDialogue.js
@@ -3,13 +3,42 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const StyledDialogue = styled.div`
+  padding: 2rem 3rem;
+  background-color: ${props => props.theme.container};
   z-index: 100;
+`;
+const StyledHeading = styled.h2`
+  margin-top: 0;
+`;
+const StyledActions = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+const StyledButton = styled.button`
+  display: block;
+  padding: 0.6rem 1.5rem;
+  margin: 2rem auto 0;
+  border: none;
+  color: ${props => props.theme.text};
+  background-color: ${props => props.theme.accent};
+  cursor: pointer;
+  transition: 0.2s color;
+  &:hover {
+    color: ${props => props.theme.lightPrimary};
+  }
+`;
+const StyledYesButton = styled(StyledButton)`
+  background-color: ${props => props.theme.primary};
 `;
 
 const DeleteDialogue = () => {
   return (
     <StyledDialogue>
-      <h1>Delete this motherfucker!</h1>
+      <StyledHeading>Delete this project?</StyledHeading>
+      <StyledActions>
+        <StyledYesButton>Yes</StyledYesButton>
+        <StyledButton>No</StyledButton>
+      </StyledActions>
     </StyledDialogue>
   );
 };
