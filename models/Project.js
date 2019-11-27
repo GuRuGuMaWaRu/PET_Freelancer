5972a0bf23e4b52de1dbf1cb66806510e319355b
GuRuGu <peter.k.nets@gmail.com>
2019-11-27 11:06:34 +0200

    Backend --- add User model & update Client and Project models

--- models/Project.js
+++ models/Project.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ProjectSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
   client: {
     type: Schema.Types.ObjectId,
     ref: "Client"
