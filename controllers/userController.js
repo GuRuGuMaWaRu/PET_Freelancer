c99fe43b2dc84a6ff1b6435edc0e5a5d1d58cffb
GuRuGu <peter.k.nets@gmail.com>
2019-11-27 15:14:56 +0200

    Backend --- write registration route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,59 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { validationResult } = require("express-validator");
+
+const User = require("../models/User");
+
 module.exports = {
   post: async (req, res) => {
+    // Handle Registration form errors
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: "User already exists " });
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user._id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        process.env.JWTSECRET,
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.status(201).json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).json({ msg: err.message });
+    }
+
     console.log("User POST route");
   }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator");
 
 const userController = require("../controllers/userController");
 
 // @route     POST users/
 // @desc      Register a user
 // @access    Public
-router.post("/", userController.post);
+router.post(
+  "/",
+  [
+    check("name", "Please add name")
+      .not()
+      .isEmpty(),
+    check("email", "Please add email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  userController.post
+);
 
 module.exports = router;
