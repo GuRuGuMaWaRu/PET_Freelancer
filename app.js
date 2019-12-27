0e5e5bfd9b2aedbe0f963775ded7cbd2f5df8327
GuRuGu <peter.k.nets@gmail.com>
2019-12-27 09:04:47 +0200

    Prepare for deployment to Heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
-// const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const usersRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
@@ -9,10 +9,15 @@ const clientsRouter = require("./routes/clients");
 const projectsRouter = require("./routes/projects");
 
 // Set environament variables
-require("dotenv").config({ path: "process.env" });
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: "process.env" });
+}
 
 // Connect to mongo DB
-if (process.env.NODE_ENV === "development") {
+if (
+  process.env.NODE_ENV === "development" ||
+  process.env.NODE_ENV === "production"
+) {
   mongoose.connect(process.env.DB_MAIN, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
@@ -22,15 +27,18 @@ if (process.env.NODE_ENV === "development") {
   mongoose.set("useCreateIndex", true);
 }
 
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => console.log("Connection to database is established"));
+
 const app = express();
 
+// Set up middleware
 app.use(logger("dev"));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
 
+// Set CORS headers so that React SPA is able to communicate with this server
 app.use((req, res, next) => {
-  // Set CORS headers so that React SPA is able to communicate with this server
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
@@ -40,10 +48,20 @@ app.use((req, res, next) => {
   next();
 });
 
+// Set up routes
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
 app.use("/projects", projectsRouter);
 app.use("/clients", clientsRouter);
+
+// Heroku deployment --- serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 // Handle 404 errors
 app.use((req, res, next) => {
@@ -51,6 +69,7 @@ app.use((req, res, next) => {
   error.status = 404;
   next(error);
 });
+
 // Handle all errors
 app.use((error, req, res) => {
   res.status(error.status || 500);
