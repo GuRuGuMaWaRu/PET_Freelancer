d83f2716952fbe39d536284d87fee515b3a63876
GuRuGu <peter.k.nets@gmail.com>
2020-03-24 16:06:20 +0200

    Server---use path module to resolve paths

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use((req, res, next) => {
 
 // Heroku deployment --- serve static assets in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client", "build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
