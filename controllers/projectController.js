d2775d9cb23680d45afa56cbeee24db296f2b268
GuRuGu <peter.k.nets@gmail.com>
2020-04-10 14:19:23 +0300

    Solve Project-Add error

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -74,8 +74,7 @@ exports.getProject = catchAsync(async (req, res, next) => {
   const project = await Project.findOne({
     _id: req.params.id,
     user: req.user.id
-  });
-  // .select("client currency date payment projectNr _id");
+  }).select("client currency date payment projectNr _id");
 
   if (!project) {
     return next(new AppError("No project found with this ID", 404));
