63e1f506c370af90ea80298c90801e0a5480a605
GuRuGu <peter.k.nets@gmail.com>
2020-05-10 12:33:25 +0300

    Add a generic createProject route

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -44,6 +44,15 @@ exports.getProject = catchAsync(async (req, res, next) => {
   });
 });
 
+// @route     POST projects/
+// @desc      Create project
+// @access    Private
+exports.createProject = catchAsync(async (req, res, next) => {
+  const newProject = Project.create({ ...req.body, user: req.userId });
+
+  res.status(201).json({ status: "success", data: { data: newProject } });
+});
+
 // @route     POST projects/withClient/
 // @desc      Create project and (possibly) client
 // @access    Private
