a3db71a15ca7822479359802c4868fc85846f489
GuRuGu <peter.k.nets@gmail.com>
2020-05-11 14:19:45 +0300

    Add getOne handler

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -15,3 +15,23 @@ exports.getAll = Model =>
       .status(200)
       .json({ status: "success", results: docs.length, data: { data: docs } });
   });
+
+exports.getOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const filter = { _id: req.params.id };
+
+    if (req.userId) {
+      filter.user = req.userId;
+    }
+
+    const doc = await Model.findOne(filter);
+
+    if (!doc) {
+      return next(new AppError("No doc found with this ID", 404));
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: { data: doc }
+    });
+  });
