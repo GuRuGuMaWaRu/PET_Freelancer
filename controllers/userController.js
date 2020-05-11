4cb97c1fe7b486b3075ecbf1c33e38c531cb42b5
GuRuGu <peter.k.nets@gmail.com>
2020-05-11 13:47:12 +0300

    Use getAllDocs handler inside userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,14 @@
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
+const factory = require("./handlerFactory");
 
 //--> Admin routes
 
 // @route     GET users/
 // @desc      Get all users
 // @access    Private
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-
-  res
-    .status(200)
-    .json({ status: "success", results: users.length, data: { data: users } });
-});
+exports.getAllUsers = factory.getAllDocs(User);
 // @route     GET users/:id
 // @desc      Get user
 // @access    Private
