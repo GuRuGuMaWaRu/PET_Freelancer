af3fb85d7ebb4e2bd99020defcf547798e18b3b3
GuRuGu <peter.k.nets@gmail.com>
2020-05-15 08:54:27 +0300

    Add updateOne generic method

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -35,3 +35,20 @@ exports.getOne = Model =>
       data: { data: doc }
     });
   });
+
+exports.updateOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!doc) {
+      return next(new AppError("No doc found with this ID", 404));
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: { data: doc }
+    });
+  });
