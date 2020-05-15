1c44621d19186ccf35d7c03a4bad250b36f34d88
GuRuGu <peter.k.nets@gmail.com>
2020-05-15 14:32:40 +0300

    Remove unused imports

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,6 +1,5 @@
 const Client = require("../models/clientModel");
 const catchAsync = require("../utils/catchAsync");
-const AppError = require("../utils/appError");
 const factory = require("./handlerFactory");
 
 // @route     GET clients/
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,5 @@
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
-const AppError = require("../utils/appError");
 const factory = require("./handlerFactory");
 
 //--> Admin routes
