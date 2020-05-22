2d07033bc7c38a0556abaabfe58ea23030c15ebb
GuRuGu <peter.k.nets@gmail.com>
2020-05-22 17:52:09 +0300

    Create Add Client miniform

---
+++ client/src/components/project_form/AddClient.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from "prop-types";
+import * as Yup from "yup";
+import { Formik } from "formik";
+
+import {
+  StyledForm,
+  StyledFormGroup,
+  StyledLabel,
+  StyledField,
+  StyledErrorMessage,
+  StyledAddClientGroup,
+  StyledAddClientButton
+} from "./formStyles";
+
+const formSchema = Yup.object().shape({
+  client: Yup.string()
+});
+
+const AddClient = () => {
+  return (
+    <Formik
+      initialValues={{ client: "" }}
+      validationSchema={formSchema}
+      onSubmit={async (values, actions) => {
+        try {
+        } catch (err) {
+          console.log(err);
+          actions.setSubmitting(false);
+          actions.setStatus({ msg: "Something went wrong" });
+        }
+      }}
+      render={({ errors, status, touched, isSubmitting }) => (
+        <StyledForm>
+          <StyledFormGroup>
+            <StyledLabel htmlFor="client">New Client:</StyledLabel>
+            <StyledAddClientGroup>
+              <StyledField type="text" name="client" />
+              <StyledAddClientButton>Add</StyledAddClientButton>
+            </StyledAddClientGroup>
+            <StyledErrorMessage name="client" component="div" />
+          </StyledFormGroup>
+        </StyledForm>
+      )}
+    />
+  );
+};
+
+AddClient.propTypes = {};
+
+export default AddClient;
