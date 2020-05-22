7422c6c127198ca9deedec2688b534507c2a9622
GuRuGu <peter.k.nets@gmail.com>
2020-05-22 17:57:48 +0300

    Remove proptypes from AddClient form

--- client/src/components/project_form/AddClient.js
+++ client/src/components/project_form/AddClient.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import * as Yup from "yup";
 import { Formik } from "formik";
 
@@ -47,7 +46,5 @@ const AddClient = () => {
     />
   );
 };
-
-AddClient.propTypes = {};
 
 export default AddClient;
