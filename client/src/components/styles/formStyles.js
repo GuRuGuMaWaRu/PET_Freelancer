fa086952353635734668604f43ec57d5fb7ce5ae
GuRuGu <peter.k.nets@gmail.com>
2020-05-28 09:57:59 +0300

    Change formStyles file structure

--- client/src/components/styles/formStyles.js
+++ client/src/components/styles/formStyles.js
@@ -1,45 +1,59 @@
 import styled from "styled-components";
 import { Form, Field, ErrorMessage } from "formik";
 
+/* General */
+
 export const StyledForm = styled(Form)`
   padding: 1rem 2rem;
 `;
+
 export const StyledTitle = styled.h1`
   font-size: 2.5rem;
   text-align: center;
   padding-top: 1rem;
   margin-top: 0;
 `;
+
 export const StyledFormGroup = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   padding: 0.5rem;
 `;
+
 export const StyledLabel = styled.label`
   display: flex;
   justify-content: flex-end;
   align-items: center;
   margin-right: 0.5rem;
 `;
+
 export const StyledField = styled(Field)`
   max-width: 300px;
   padding: 0.2rem;
 `;
+
+/* Messages */
+
 export const StyledErrorMessage = styled(ErrorMessage)`
   grid-column-start: 2;
   margin-top: 0.4rem;
   color: ${props => props.theme.primary};
 `;
+
 export const StyledStatusMessage = styled.div`
   grid-column-start: 2;
   margin-top: 0.4rem;
   color: ${props => props.theme.primary};
 `;
+
+/* Buttons */
+
 export const StyledActionButtons = styled.div`
   display: flex;
   justify-content: center;
   margin: 2rem auto 0;
 `;
+
 export const StyledButton = styled.button`
   display: block;
   padding: 0.6rem 1.5rem;
@@ -51,17 +65,21 @@ export const StyledButton = styled.button`
     color: ${props => props.theme.lightPrimary};
   }
 `;
+
 export const StyledSubmitButton = styled(StyledButton)`
   background-color: ${props => props.theme.mediumseagreen};
 `;
+
 export const StyledCancelButton = styled(StyledButton)`
   margin-right: 1rem;
   background-color: ${props => props.theme.secondaryText};
 `;
+
 export const StyledAddClientGroup = styled.div`
   display: flex;
   width: 300px;
 `;
+
 export const StyledAddClientButton = styled.button`
   flex: 2;
   border: none;
