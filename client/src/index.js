8a1c3742a7071dfa696e1a87fd8a5b65501220d8
GuRuGu <peter.k.nets@gmail.com>
2020-06-30 09:26:24 +0300

    User HMR for App

--- client/src/index.js
+++ client/src/index.js
@@ -6,27 +6,36 @@ import { Provider } from "react-redux";
 import theme from "./components/styles/theme";
 import GlobalStyles from "./components/styles/global.styles";
 
-import App from "./components/app/App";
 import ProjectState from "./context/project/ProjectState";
 import ClientState from "./context/client/ClientState";
 import AlertState from "./context/alert/AlertState";
 import AuthState from "./context/auth/AuthState";
 import store from "./store";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <AuthState>
-      <ProjectState>
-        <ClientState>
-          <AlertState>
-            <GlobalStyles />
-            <ThemeProvider theme={theme}>
-              <App />
-            </ThemeProvider>
-          </AlertState>
-        </ClientState>
-      </ProjectState>
-    </AuthState>
-  </Provider>,
-  document.getElementById("root")
-);
+const render = () => {
+  const App = require("./components/app/App").default;
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <AuthState>
+        <ProjectState>
+          <ClientState>
+            <AlertState>
+              <GlobalStyles />
+              <ThemeProvider theme={theme}>
+                <App />
+              </ThemeProvider>
+            </AlertState>
+          </ClientState>
+        </ProjectState>
+      </AuthState>
+    </Provider>,
+    document.getElementById("root")
+  );
+};
+
+render();
+
+if (process.env.NODE_ENV === "development" && module.hot) {
+  module.hot.accept("./components/app/App", render);
+}
