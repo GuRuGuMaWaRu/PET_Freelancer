c5beab365f74bf587295560142ebe6b4bc6ab70d
GuRuGu <peter.k.nets@gmail.com>
2020-07-13 15:45:59 +0300

    Remove all context-related code from FilterList

--- client/src/components/projects/FilterList.js
+++ client/src/components/projects/FilterList.js
@@ -1,18 +1,13 @@
-import React, { useContext } from "react";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import Filter from "./Filter";
-// import ClientContext from "../../context/client/clientContext";
 import { toggleFilter } from "../../reducers/filtersSlice";
-
 import { StyledFilterList } from "../styles/filter.styles";
 
 const FilterList = () => {
   const filterableProps = useSelector(state => state.filters);
   const dispatch = useDispatch();
-
-  // const clientContext = useContext(ClientContext);
-  // const { toggleFilter } = clientContext;
 
   const renderedFilters = Object.values(filterableProps).reduce(
     (final, filters) => [...final, ...filters],
