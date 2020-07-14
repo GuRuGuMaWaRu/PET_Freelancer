992830030dc6624b79e0b235332d80be47f0a44a
GuRuGu <peter.k.nets@gmail.com>
2020-07-14 10:59:19 +0300

    Use new alerts slice to show client-creation alerts

--- client/src/components/layout/Alerts.js
+++ client/src/components/layout/Alerts.js
@@ -1,20 +1,24 @@
 import React, { useContext } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { TransitionGroup, Transition } from "react-transition-group";
 
 import Alert from "./Alert";
-import AlertContext from "../../context/alert/alertContext";
+// import AlertContext from "../../context/alert/alertContext";
+import { removeAlert, selectAllAlerts } from "../../reducers/alertsSlice";
 // import AuthContext from "../../context/auth/authContext";
 import { StyledAlertList } from "../styles/alert.styles";
 
 const duration = 500;
 
 const Alerts = () => {
-  const alertContext = useContext(AlertContext);
+  const alerts = useSelector(selectAllAlerts);
+  const dispatch = useDispatch();
+  // const alertContext = useContext(AlertContext);
   // const authContext = useContext(AuthContext);
 
-  const { alerts, removeAlert } = alertContext;
+  // const { alerts, removeAlert } = alertContext;
   // const { error, hideError } = authContext;
-  const handleRemoveAlert = id => removeAlert(id);
+  // const handleRemoveAlert = id => dispatch(removeAlert(id));
 
   return (
     <TransitionGroup component={StyledAlertList}>
@@ -25,7 +29,7 @@ const Alerts = () => {
               alert={alert}
               state={state}
               duration={duration}
-              removeAlert={handleRemoveAlert}
+              removeAlert={() => dispatch(removeAlert(alert.id))}
             />
           )}
         </Transition>
--- client/src/reducers/alertsSlice.js
+++ client/src/reducers/alertsSlice.js
@@ -1,16 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
+import uuid from "uuid";
 
-const initialState = [];
+import { createClient } from "./clientsSlice";
+
+export const alertsAdapter = createEntityAdapter();
+
+const initialState = alertsAdapter.getInitialState();
 
 export const slice = createSlice({
   name: "alerts",
   initialState,
   reducers: {
-    addAlert(state, action) {},
-    removeAlert(state, action) {}
+    addAlert(state, action) {
+      alertsAdapter.addOne(state, { id: uuid(), ...action.payload });
+    },
+    removeAlert(state, action) {
+      alertsAdapter.removeOne(state, action.payload);
+    }
+  },
+  extraReducers: builder => {
+    builder.addCase(createClient.fulfilled, (state, action) => {
+      alertsAdapter.addOne(state, {
+        id: uuid(),
+        type: "info",
+        msg: `Created client: ${action.payload.name}`
+      });
+    });
   }
 });
 
 export const { addAlert, removeAlert } = slice.actions;
+
+export const { selectAll: selectAllAlerts } = alertsAdapter.getSelectors(
+  state => state.alerts
+);
 
 export default slice.reducer;
