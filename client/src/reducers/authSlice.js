50a7e1c5aa823ef9386ccc0393869e3f513e9699
GuRuGu <peter.k.nets@gmail.com>
2020-07-27 11:27:41 +0300

    Implement getUser action

--- client/src/reducers/authSlice.js
+++ client/src/reducers/authSlice.js
@@ -1,17 +1,40 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+export const getUser = createAsyncThunk(
+  "auth/getUser",
+  async (_, { rejectWithValue }) => {
+    try {
+      const res = await axios.get("/api/v1/auth");
+      localStorage.setItem("freelancer_token", res.data.token);
+
+      return res.data.data;
+    } catch (err) {
+      return rejectWithValue(err.message);
+    }
+  }
+);
+
 const initialState = {
   isAuthenticated: false,
   currentUser: null,
-  loadingUser: true
+  loading: false
 };
 
 export const slice = createSlice({
   name: "auth",
   initialState,
   reducers: {},
-  extraReducers: builder => {}
+  extraReducers: builder => {
+    builder.addCase(getUser.pending, (state, _) => {
+      state.loading = true;
+    });
+    builder.addCase(getUser.fulfilled, (state, action) => {
+      state.isAuthenticated = true;
+      state.currentUser = action.payload.user;
+      state.loading = false;
+    });
+  }
 });
 
 export const {} = slice.actions;
