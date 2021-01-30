b2c447ba3daa429be6a45d674bfc5b1a7ef932b8
GuRuGu <peter.k.nets@gmail.com>
2021-01-30 10:12:46 +0200

    Fix module exports for clientController

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -4,24 +4,32 @@ const factory = require("./handlerFactory");
 // @route     GET clients/
 // @desc      Get all clients
 // @access    Private
-exports.getAllClients = factory.getAll(Client);
+const getAllClients = factory.getAll(Client);
 
 // @route     GET clients/:id
 // @desc      Get client
 // @access    Private
-exports.getClient = factory.getOne(Client);
+const getClient = factory.getOne(Client);
 
 // @route     POST clients/
 // @desc      Create client
 // @access    Private
-exports.createClient = factory.createOne(Client);
+const createClient = factory.createOne(Client);
 
 // @route     PATCH clients/:id
 // @desc      Update client
 // @access    Private
-exports.updateClient = factory.updateOne(Client);
+const updateClient = factory.updateOne(Client);
 
 // @route     DELETE clients/:id
 // @desc      Delete client
 // @access    Private
-exports.deleteClient = factory.deleteOne(Client);
+const deleteClient = factory.deleteOne(Client);
+
+module.exports = {
+  getAllClients,
+  getClient,
+  createClient,
+  updateClient,
+  deleteClient
+};
