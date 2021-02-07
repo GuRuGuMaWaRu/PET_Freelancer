9c6c34c079dc184937e84c16fac2fad7855e27d5
GuRuGu <peter.k.nets@gmail.com>
2021-02-07 10:46:29 +0200

    Remove withClient controllers and routes - deprecated

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,7 +1,4 @@
-const Client = require("../models/clientModel");
 const Project = require("../models/projectModel");
-const catchAsync = require("../utils/catchAsync");
-const AppError = require("../utils/appError");
 const factory = require("./handlerFactory");
 
 // @route     GET projects/
@@ -19,99 +16,10 @@ const getProject = factory.getOne(Project);
 // @access    Private
 const createProject = factory.createOne(Project);
 
-// @route     POST projects/withClient/
-// @desc      Create project and (possibly) client
-// @access    Private
-const createProjectWithClient = catchAsync(async (req, res, next) => {
-  let newClient = null;
-
-  //--> Create new client OR get existing client id
-  if (req.body.newClient && req.body.newClient.trim().length > 0) {
-    const existingClient = await Client.findOne({
-      name: req.body.newClient,
-      user: req.userId
-    });
-
-    if (!existingClient) {
-      newClient = new Client({
-        name: req.body.newClient,
-        user: req.userId
-      });
-
-      req.body.client = newClient._id;
-
-      await newClient.save();
-    } else {
-      req.body.client = existingClient._id;
-    }
-  }
-  //--> Create & save new project
-  const project = new Project({
-    ...req.body,
-    user: req.userId
-  });
-  await project.save();
-
-  //--> Get newly created project from DB
-  const newProject = await Project.findOne({
-    _id: project._id,
-    user: req.userId
-  }).populate({ path: "client", select: "-_id" });
-
-  res.status(201).json({ status: "success", data: { newProject, newClient } });
-});
-
 // @route     PATCH projects/:id
 // @desc      Update project
 // @access    Private
 const updateProject = factory.updateOne(Project);
-
-// @route     PATCH projects/:id/withClient/
-// @desc      Update project and (possibly) client
-// @access    Private
-const updateProjectWithClient = catchAsync(async (req, res, next) => {
-  let newClient = null;
-
-  //--> Create new client OR get existing client id
-  if (req.body.newClient && req.body.newClient.trim().length > 0) {
-    const existingClient = await Client.findOne({
-      name: req.body.newClient,
-      user: req.userId
-    });
-
-    if (!existingClient) {
-      newClient = new Client({
-        name: req.body.newClient,
-        user: req.userId
-      });
-
-      req.body.client = newClient._id;
-
-      await newClient.save();
-    } else {
-      req.body.client = existingClient._id;
-    }
-  }
-
-  //--> Update project
-  const updatedProject = await Project.findOneAndUpdate(
-    {
-      _id: req.params.id,
-      user: req.userId
-    },
-    req.body,
-    { new: true }
-  );
-
-  if (!updatedProject) {
-    return next(new AppError("No project found with this ID", 404));
-  }
-
-  res
-    .status(200)
-    .json({ status: "success", data: { updatedProject, newClient } });
-  // .json({ status: "success", data: null });
-});
 
 // @route     DELETE projects/:id
 // @desc      Delete project
@@ -122,8 +30,6 @@ module.exports = {
   getAllProjects,
   getProject,
   createProject,
-  createProjectWithClient,
   updateProject,
-  updateProjectWithClient,
   deleteProject
 };
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -33,18 +33,4 @@ router
   // @access    Private
   .delete(projectController.deleteProject);
 
-//--> Specialized routes
-router
-  .route("/withClient")
-  // @route     POST projects/withClient/
-  // @desc      Create project and (possibly) client
-  // @access    Private
-  .post(projectController.createProjectWithClient);
-
-router
-  .route("/:id/withClient") // @route     PATCH projects/withClient/:id
-  // @desc      Update project and (possibly) client
-  // @access    Private
-  .patch(projectController.updateProjectWithClient);
-
 module.exports = router;
