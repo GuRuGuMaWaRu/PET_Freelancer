f98b3ede536c53c21cf342fbda59a677186e68d1
GuRuGu <peter.k.nets@gmail.com>
2021-02-18 13:38:45 +0200

    Fix typo in filename

--- resources/project/project.router.js
+++ resources/project/project.router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const auth = require("../../middleware/auth");
-const projectControllers = require("./project.controller");
+const projectControllers = require("./project.controllers");
 
 const router = express.Router();
 
