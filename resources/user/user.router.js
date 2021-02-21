cd8af8e369a5e77b4568b0bc2a6f4adf2535b1f7
GuRuGu <peter.k.nets@gmail.com>
2021-02-21 09:41:23 +0200

    Restore user queries

---
+++ resources/user/user.router.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const { protect } = require("../../utils/auth");
+const userControllers = require("./user.controllers");
+
+const router = express.Router();
+
+router.use(protect);
+
+router
+  .route("/")
+  // @route     GET users/
+  // @desc      Get all users
+  // @access    Private
+  .get(userControllers.getAll);
+
+router
+  .route("/:id")
+  // @route     GET users/:id
+  // @desc      Get user
+  // @access    Private
+  .get(userControllers.getOne)
+  // @route     DELETE users/:id
+  // @desc      Delete user
+  // @access    Private
+  .delete(userControllers.deleteOne);
+
+module.exports = router;
