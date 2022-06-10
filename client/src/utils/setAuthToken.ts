7e8fef2dc58aa017eafffa63bdac53df38d6bc7b
GuRuGu <peter.k.nets@gmail.com>
2022-06-10 18:16:39 +0300

    Update setAuthToken args types

--- client/src/utils/setAuthToken.ts
+++ client/src/utils/setAuthToken.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const setAuthToken = (token: string): void => {
+const setAuthToken = (token: string | null): void => {
   if (token) {
     axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   } else {
