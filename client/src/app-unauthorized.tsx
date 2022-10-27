0883168e5cc49307fdade9a1d6f138229706ea2f
GuRuGu <peter.k.nets@gmail.com>
2022-10-27 09:09:25 +0300

    Move unauthorized path into a separate file

---
+++ client/src/app-unauthorized.tsx
@@ -0,0 +1,57 @@
+/** @jsxImportSource @emotion/react */
+import * as colors from "./styles/colors";
+import {
+  Button,
+  Modal,
+  ModalOpenButton,
+  ModalContents,
+  LoginForm,
+  RegisterForm,
+  Notification,
+} from "./components";
+
+function AppUnauthorized() {
+  return (
+    <>
+      <Notification />
+      <div
+        css={{
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: colors.base,
+        }}
+      >
+        <h1 css={{ color: "#fff", fontSize: "3em" }}>Freelancer</h1>
+        <div
+          css={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            columnGap: "20px",
+          }}
+        >
+          <Modal>
+            <ModalOpenButton>
+              <Button>Login</Button>
+            </ModalOpenButton>
+            <ModalContents aria-label="Login Form" title="Login">
+              <LoginForm submitButton={<Button>Login</Button>} />
+            </ModalContents>
+          </Modal>
+          <Modal>
+            <ModalOpenButton>
+              <Button variant="secondary">Register</Button>
+            </ModalOpenButton>
+            <ModalContents aria-label="Register Form" title="Register">
+              <RegisterForm submitButton={<Button>Register</Button>} />
+            </ModalContents>
+          </Modal>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export { AppUnauthorized };
--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,57 +1,12 @@
 /** @jsxImportSource @emotion/react */
-import * as colors from "./styles/colors";
-import {
-  Button,
-  Modal,
-  ModalOpenButton,
-  ModalContents,
-  LoginForm,
-  RegisterForm,
-  Notification,
-} from "./components";
+
+import { useAuth } from "./context";
+import { AppUnauthorized } from "./app-unauthorized";
 
 function App() {
-  return (
-    <>
-      <Notification />
-      <div
-        css={{
-          height: "100vh",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: colors.base,
-        }}
-      >
-        <h1 css={{ color: "#fff", fontSize: "3em" }}>Freelancer</h1>
-        <div
-          css={{
-            display: "grid",
-            gridTemplateColumns: "1fr 1fr",
-            columnGap: "20px",
-          }}
-        >
-          <Modal>
-            <ModalOpenButton>
-              <Button>Login</Button>
-            </ModalOpenButton>
-            <ModalContents aria-label="Login Form" title="Login">
-              <LoginForm submitButton={<Button>Login</Button>} />
-            </ModalContents>
-          </Modal>
-          <Modal>
-            <ModalOpenButton>
-              <Button variant="secondary">Register</Button>
-            </ModalOpenButton>
-            <ModalContents aria-label="Register Form" title="Register">
-              <RegisterForm submitButton={<Button>Register</Button>} />
-            </ModalContents>
-          </Modal>
-        </div>
-      </div>
-    </>
-  );
+  const { user } = useAuth();
+
+  return user ? <div>You are in!</div> : <AppUnauthorized />;
 }
 
 export { App };
