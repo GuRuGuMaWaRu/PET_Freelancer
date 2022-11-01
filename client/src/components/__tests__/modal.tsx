9bc054042fbfffa42a56ca8e8939054999b01259
GuRuGu <peter.k.nets@gmail.com>
2022-11-01 09:52:56 +0200

    Test Modal component

---
+++ client/src/components/__tests__/modal.tsx
@@ -0,0 +1,39 @@
+import {
+  render,
+  screen,
+  within,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import { Modal, ModalOpenButton, ModalContents, Button } from "../";
+
+test("can be opened and closed", async () => {
+  const label = "Modal label";
+  const title = "Modal title";
+  const user = userEvent.setup();
+
+  render(
+    <Modal>
+      <ModalOpenButton>
+        <Button>Open</Button>
+      </ModalOpenButton>
+      <ModalContents aria-label={label} title={title}>
+        <div>Modal Content</div>
+      </ModalContents>
+    </Modal>,
+  );
+
+  await user.click(screen.getByRole("button", { name: /open/i }));
+
+  expect(screen.getByRole("dialog")).toHaveAttribute("aria-label", label);
+
+  const inModal = within(screen.getByRole("dialog"));
+  expect(inModal.getByRole("heading", { name: title })).toBeInTheDocument();
+  expect(inModal.getByText(/modal content/i)).toBeInTheDocument();
+  expect(inModal.getByRole("button", { name: /close/i })).toBeInTheDocument();
+
+  await user.click(inModal.getByRole("button", { name: /close/i }));
+  await waitForElementToBeRemoved(screen.queryByRole("dialog"));
+  expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+});
