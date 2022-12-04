392d42c867bedd688497510edeef4dbb83f5ee30
GuRuGu <peter.k.nets@gmail.com>
2022-12-04 09:31:09 +0200

    Authorize full-year-of-projects request

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,12 +1,15 @@
 import { client } from "./";
 import type { IProject } from './'
+import { localStorageKey } from "../config";
 
 const getAllProjects = async () => {
   return await client<IProject[]>("projects");
 };
 
 const getProjectsForYear = async () => {
-  return await client<IProject[]>("projects/lastYear");
+  const token = window.localStorage.getItem(localStorageKey);
+
+  return await client<IProject[]>("projects/lastYear", { token: token ?? '' });
 };
 
 export { getAllProjects, getProjectsForYear };
