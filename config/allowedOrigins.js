bfe5277e0699e44f34c329e1b263127c21f6de30
GuRuGu <peter.k.nets@gmail.com>
2022-12-04 09:31:38 +0200

    Tweak the list of allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-  "http://localhost:3000",
+  "http://localhost:3000/",
+  "http://localhost:5000/",
   "https://me-freelancer-v1-duml.onrender.com/",
   "https://tranquil-woodland-50991.herokuapp.com/",
 ];
