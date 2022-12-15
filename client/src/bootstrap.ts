e2bafe1bb96abf6831b58e7e22fa9f36cafe4917
GuRuGu <peter.k.nets@gmail.com>
2022-12-15 09:19:15 +0200

    Fix - add ReachCombobox styles

--- client/src/bootstrap.ts
+++ client/src/bootstrap.ts
@@ -1,4 +1,5 @@
 import "stop-runaway-react-effects/hijack";
 import "@reach/dialog/styles.css";
 import "@reach/tooltip/styles.css";
+import "@reach/combobox/styles.css";
 import "./styles/global.css";
