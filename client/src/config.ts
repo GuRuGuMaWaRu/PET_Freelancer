45435251c3ed81ea6af5d9c95dd0a5c7471f7272
GuRuGu <peter.k.nets@gmail.com>
2022-12-16 09:16:44 +0200

    Fix formatting in config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,6 +3,7 @@ export const API_URL =
   process.env.NODE_ENV === "test"
     ? "http://localhost/api/v1"
     : "http://localhost:3000/api/v1";
+
 export const localStorageKey = "__FreelancerApp_token__";
 export const usersKey = "__FreelancerApp_dev_users__";
 export const projectsKey = "__FreelancerApp_dev_projects__";
