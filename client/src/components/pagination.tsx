9d1e489f92daf0d578ab80f85c43675a0a9cc715
GuRuGu <peter.k.nets@gmail.com>
2023-02-11 11:10:35 +0200

    feat: improve pagination

--- client/src/components/pagination.tsx
+++ client/src/components/pagination.tsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
+import styled from "@emotion/styled";
 import {
   MdOutlineArrowBackIos,
   MdOutlineArrowForwardIos,
@@ -11,19 +12,61 @@ interface IProps {
   paginationCallback: (page: number) => void;
 }
 
+const SPaginationContainer = styled.div({
+  display: "flex",
+  justifyContent: "space-between",
+  marginTop: "1rem",
+});
+
 const Pagination: React.FC<IProps> = ({
   currentPage,
   numberOfPages,
   paginationCallback,
 }) => {
+  console.log("currentPage:", currentPage);
+  console.log("numberOfPages:", numberOfPages);
+
+  let pages: { page: number; next_previous?: boolean }[] = [];
+
+  if (numberOfPages <= 9) {
+    pages = Array(numberOfPages)
+      .fill(1)
+      .map((_, index) => ({ page: index + 1 }));
+  } else {
+    if (currentPage <= 5) {
+      pages = [
+        ...Array(7)
+          .fill(1)
+          .map((_, index) => ({ page: index + 1 })),
+        { page: 8, next_previous: true },
+        { page: numberOfPages },
+      ];
+    } else if (currentPage >= numberOfPages - 4) {
+      pages = [
+        { page: 1 },
+        { page: 2, next_previous: true },
+        ...Array(7)
+          .fill(1)
+          .map((_, index) => ({ page: numberOfPages - index }))
+          .reverse(),
+      ];
+    } else {
+      pages = [
+        { page: 1 },
+        { page: currentPage - 3, next_previous: true },
+        { page: currentPage - 2 },
+        { page: currentPage - 1 },
+        { page: currentPage },
+        { page: currentPage + 1 },
+        { page: currentPage + 2 },
+        { page: currentPage + 3, next_previous: true },
+        { page: numberOfPages },
+      ];
+    }
+  }
+
   return (
-    <div
-      css={{
-        display: "flex",
-        justifyContent: "space-between",
-        marginTop: "1rem",
-      }}
-    >
+    <SPaginationContainer>
       <button
         aria-label="Previous page"
         onClick={() => paginationCallback(currentPage - 1)}
@@ -32,22 +75,20 @@ const Pagination: React.FC<IProps> = ({
         <MdOutlineArrowBackIos />
       </button>
       <div>
-        {Array(numberOfPages)
-          .fill(1)
-          .map((_, index) => (
-            <button
-              key={index}
-              css={{
-                backgroundColor: `${
-                  currentPage === index + 1 ? "tomato" : "#fff"
-                }`,
-              }}
-              onClick={() => paginationCallback(index + 1)}
-              aria-label={`{Select page ${index + 1}}`}
-            >
-              {index + 1}
-            </button>
-          ))}
+        {pages.map((item, index) => (
+          <button
+            key={item.page}
+            css={{
+              backgroundColor: `${
+                currentPage === item.page ? "tomato" : "#fff"
+              }`,
+            }}
+            onClick={() => paginationCallback(item.page)}
+            aria-label={`{Select page ${item.page}}`}
+          >
+            {item.next_previous ? "..." : item.page}
+          </button>
+        ))}
       </div>
       <button
         aria-label="Next page"
@@ -56,7 +97,7 @@ const Pagination: React.FC<IProps> = ({
       >
         <MdOutlineArrowForwardIos />
       </button>
-    </div>
+    </SPaginationContainer>
   );
 };
 
