b3c1db3d9cbe0d54021728396dc26b82ab5c74d4
GuRuGu <peter.k.nets@gmail.com>
2023-02-12 16:04:50 +0200

    feat: update Pagination styles

--- client/src/components/pagination.tsx
+++ client/src/components/pagination.tsx
@@ -18,20 +18,42 @@ const SPaginationContainer = styled.div({
   marginTop: "1rem",
 });
 
+interface SButtonProps {
+  disabled: boolean;
+}
+
+const SButton = styled.button<SButtonProps>(({ disabled }) => ({
+  width: "3rem",
+  padding: ".7rem",
+  backgroundColor: "transparent",
+  border: `2px solid ${disabled ? "#858585" : "#eae2e2"}`,
+  color: "#fff",
+  transition: "all 0.2s",
+  cursor: disabled ? "default" : "pointer",
+
+  "&:hover": {
+    transform: disabled ? "none" : "translateY(-2px)",
+  },
+  "& svg": {
+    fill: disabled ? "#858585" : "#eae2e2",
+    verticalAlign: "middle",
+  },
+}));
+
+const createEmptyArray = (numOfItems: number): number[] => {
+  return Array(numOfItems).fill(0);
+};
+
 const getPages = (
   currentPage: number,
   pages: number,
 ): { page: number; next_previous?: boolean }[] => {
   if (pages <= 9) {
-    return Array(pages)
-      .fill(1)
-      .map((_, index) => ({ page: index + 1 }));
+    return createEmptyArray(pages).map((_, index) => ({ page: index + 1 }));
   } else {
     if (currentPage <= 5) {
       return [
-        ...Array(7)
-          .fill(1)
-          .map((_, index) => ({ page: index + 1 })),
+        ...createEmptyArray(7).map((_, index) => ({ page: index + 1 })),
         { page: 8, next_previous: true },
         { page: pages },
       ];
@@ -39,8 +61,7 @@ const getPages = (
       return [
         { page: 1 },
         { page: 2, next_previous: true },
-        ...Array(7)
-          .fill(1)
+        ...createEmptyArray(7)
           .map((_, index) => ({ page: pages - index }))
           .reverse(),
       ];
@@ -72,36 +93,37 @@ const Pagination: React.FC<IProps> = ({
 
   return (
     <SPaginationContainer>
-      <button
+      <SButton
         aria-label="Previous page"
         onClick={() => paginationCallback(currentPage - 1)}
         disabled={currentPage === 1}
       >
         <MdOutlineArrowBackIos />
-      </button>
-      <div>
+      </SButton>
+      <div css={{ display: "flex", gap: ".3rem" }}>
         {pages.map((item) => (
-          <button
+          <SButton
             key={item.page}
             css={{
               backgroundColor: `${
-                currentPage === item.page ? "tomato" : "#fff"
+                currentPage === item.page ? "tomato" : "transparent"
               }`,
             }}
             onClick={() => paginationCallback(item.page)}
             aria-label={`{Select page ${item.page}}`}
+            disabled={currentPage === item.page}
           >
             {item.next_previous ? "..." : item.page}
-          </button>
+          </SButton>
         ))}
       </div>
-      <button
+      <SButton
         aria-label="Next page"
         onClick={() => paginationCallback(currentPage + 1)}
         disabled={currentPage === numberOfPages}
       >
         <MdOutlineArrowForwardIos />
-      </button>
+      </SButton>
     </SPaginationContainer>
   );
 };
