055b5ba700a63b6437a80da320cfe70e92b7c3a4
GuRuGu <peter.k.nets@gmail.com>
2023-02-17 14:55:45 +0200

    feat: cancel search on Projects page

--- client/src/routes/projects.tsx
+++ client/src/routes/projects.tsx
@@ -19,6 +19,7 @@ import {
   ModalContents,
   AddProjectForm,
   Pagination,
+  Spinner,
 } from "../components";
 import { PAGE_LIMIT } from "../config";
 import { useNotification } from "../context";
@@ -111,6 +112,7 @@ function Projects() {
   const [page, setPage] = React.useState<number>(1);
   const [sortColumn, setSortColumn] = React.useState<string>("-date");
   const [sortDir, setSortDir] = React.useState<string>("");
+  const [searchInput, setSearchInput] = React.useState<string>("");
   const [searchQuery, setSearchQuery] = React.useState<string>("");
 
   const { data: clients = [] } = useQuery(getAllClientsQuery());
@@ -125,20 +127,22 @@ function Projects() {
     setSortDir((prevDir) => (prevDir === "" ? "-" : ""));
   };
 
-  const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-    const target = event.target as HTMLFormElement;
-    const q = target.search.value;
-
-    if (q.length >= 3 || q.length === 0) {
-      setSearchQuery(q);
+    if (searchInput.length >= 3 || searchInput.length === 0) {
+      setSearchQuery(searchInput);
     } else {
       setNotification({
         type: NotificationType.warning,
-        message: "Enter at least 3 characters",
+        message: "Enter at least 3 characters into Search field",
       });
     }
+  };
+
+  const handleCancelSearch = () => {
+    setSearchInput("");
+    setSearchQuery("");
   };
 
   //** Calculate total number of pages */
@@ -148,10 +152,42 @@ function Projects() {
     <>
       <SContainer>
         <form
-          css={{ display: "flex", alignItems: "center" }}
+          css={{
+            display: "flex",
+            alignItems: "center",
+          }}
           onSubmit={(e) => handleSearch(e)}
         >
-          <input placeholder="Search projects..." id="search" type="search" />
+          <input
+            placeholder="Search projects..."
+            id="search"
+            type="search"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          {searchInput.length > 0 && (
+            <Tooltip label="Cancel search">
+              <label htmlFor="cancel">
+                <button
+                  css={{
+                    color: "tomato",
+                    border: "0",
+                    position: "relative",
+                    marginLeft: "-30px",
+                    background: "transparent",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "30px",
+                    height: "30px",
+                  }}
+                  onClick={handleCancelSearch}
+                >
+                  <FaTimes aria-label="cancel" />
+                </button>
+              </label>
+            </Tooltip>
+          )}
           <Tooltip label="Search projects">
             <label htmlFor="search">
               <button
@@ -159,12 +195,19 @@ function Projects() {
                 css={{
                   border: "0",
                   position: "relative",
-                  marginLeft: "-35px",
                   background: "transparent",
-                  verticalAlign: "middle",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "30px",
+                  height: "30px",
                 }}
               >
-                <FaSearch aria-label="search" />
+                {projectsIsFetching ? (
+                  <Spinner />
+                ) : (
+                  <FaSearch aria-label="search" />
+                )}
               </button>
             </label>
           </Tooltip>
