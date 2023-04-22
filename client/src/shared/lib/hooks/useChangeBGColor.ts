de3014e415e0ac9aa517383cb7321a6d0ca7a401
GuRuGu <peter.k.nets@gmail.com>
2023-04-22 15:56:10 +0300

    docs: add a refactor todo for future self

--- client/src/shared/lib/hooks/useChangeBGColor.ts
+++ client/src/shared/lib/hooks/useChangeBGColor.ts
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 
 import { colors } from "../../const";
 
+// TODO: maybe I should move this hook closer to where it is used, after all it is hardly a Shared hook
 function getBgColor(pathname: string) {
   if (pathname.startsWith("/projects")) {
     return colors.greenLight1;
