01a2e6d1d1c44348ac71cb7628e4ab61a96575b7
GuRuGu <peter.k.nets@gmail.com>
2023-04-22 15:39:43 +0300

    refactor: move router into its own file

--- client/src/pages/app-authenticated/app-authenticated.tsx
+++ client/src/pages/app-authenticated/app-authenticated.tsx
@@ -1,58 +1,7 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
+import { router } from "./router";
 import { FullPageSpinner } from "../../shared/ui";
-import { Root, ErrorPage, NotFound, Dashboard, dashboardLoader } from "..";
-import {
-  Projects,
-  projectsLoader,
-  projectsAddAction,
-  projectsDeleteAction,
-  projectsEditAction,
-  Clients,
-} from "../../routes";
-import { queryClient } from "../../context";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <div>There's an error</div>,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          {
-            index: true,
-            element: <Dashboard />,
-            loader: dashboardLoader(queryClient),
-          },
-          {
-            path: "projects",
-            element: <Projects />,
-            loader: projectsLoader(queryClient),
-          },
-          {
-            path: "projects/add",
-            action: projectsAddAction(queryClient),
-          },
-          {
-            path: "projects/:projectId/delete",
-            action: projectsDeleteAction(queryClient),
-          },
-          {
-            path: "projects/:projectId/update",
-            action: projectsEditAction(queryClient),
-          },
-          {
-            path: "clients",
-            element: <Clients />,
-          },
-          { path: "*", element: <NotFound /> },
-        ],
-      },
-    ],
-  },
-]);
 
 function AppAuthenticated() {
   return (
---
+++ client/src/pages/app-authenticated/router.tsx
@@ -0,0 +1,56 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import { Root, ErrorPage, NotFound, Dashboard, dashboardLoader } from "..";
+import {
+  Projects,
+  projectsLoader,
+  projectsAddAction,
+  projectsDeleteAction,
+  projectsEditAction,
+  Clients,
+} from "../../routes";
+import { queryClient } from "../../context";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <div>There's an error</div>,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <Dashboard />,
+            loader: dashboardLoader(queryClient),
+          },
+          {
+            path: "projects",
+            element: <Projects />,
+            loader: projectsLoader(queryClient),
+          },
+          {
+            path: "projects/add",
+            action: projectsAddAction(queryClient),
+          },
+          {
+            path: "projects/:projectId/delete",
+            action: projectsDeleteAction(queryClient),
+          },
+          {
+            path: "projects/:projectId/update",
+            action: projectsEditAction(queryClient),
+          },
+          {
+            path: "clients",
+            element: <Clients />,
+          },
+          { path: "*", element: <NotFound /> },
+        ],
+      },
+    ],
+  },
+]);
+
+export { router };
