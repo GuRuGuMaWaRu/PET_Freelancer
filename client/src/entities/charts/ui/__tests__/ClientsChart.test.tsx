c2c5b3539ad0f6efb047efffa525120915427805
GuRuGu <peter.k.nets@gmail.com>
2023-05-05 14:54:04 +0300

    test: check if Clients Chart renders

---
+++ client/src/entities/charts/ui/__tests__/ClientsChart.test.tsx
@@ -0,0 +1,25 @@
+import { render } from "@testing-library/react";
+import { MemoClientsChart } from "entities/charts";
+import { getMaxLabelLength } from "entities/charts/lib";
+
+global.ResizeObserver = require("resize-observer-polyfill");
+
+jest.mock("entities/charts/lib");
+
+describe("Clients chart", () => {
+  const mockGetMaxLabelLength = getMaxLabelLength as jest.Mock;
+
+  const fakeData = [
+    { client: "Client 1", payment: 10000, projects: 10 },
+    { client: "Client 2", payment: 12000, projects: 12 },
+    { client: "Client 3", payment: 13000, projects: 13 },
+  ];
+
+  beforeEach(() => {
+    mockGetMaxLabelLength.mockReturnValue(100);
+  });
+
+  it("should render", async () => {
+    render(<MemoClientsChart data={fakeData} />);
+  });
+});
