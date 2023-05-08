888c7352fce9291c5d98793f434d7bbe71816d3f
GuRuGu <peter.k.nets@gmail.com>
2023-05-08 12:53:28 +0300

    test: test getColorBasedOnPath function

---
+++ client/src/widgets/AddProjectModal/__tests__/getColorBasedOnPath.test.ts
@@ -0,0 +1,13 @@
+import { getColorBasedOnPath } from "../AddProjectModal.helpers";
+
+describe("getColorBasedOnPath", () => {
+  it('should return "rgb(120 92 211)" for the "/" route', () => {
+    expect(getColorBasedOnPath("/")).toBe("rgb(120 92 211)");
+  });
+  it('should return "rgb(120 92 211)" as default', () => {
+    expect(getColorBasedOnPath("/abc")).toBe("rgb(120 92 211)");
+  });
+  it('should return "#4db575" for "/projects" route', () => {
+    expect(getColorBasedOnPath("/projects")).toBe("#4db575");
+  });
+});
