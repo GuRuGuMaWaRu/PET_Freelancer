fbd1843ad0c9c62921b92f7042d2eaf4471d0d12
GuRuGu <peter.k.nets@gmail.com>
2023-05-18 08:30:49 +0300

    feat: disable autoindexing DB in production

--- db.js
+++ db.js
@@ -7,8 +7,11 @@ if (process.env.NODE_ENV !== "test") {
       ? process.env.DB_DEVELOPMENT
       : process.env.DB_MAIN;
 
+  //** Disable autoIndex in production so as not to slow down the server */
+  const autoIndex = process.env.NODE_ENV === "development" ? true : false;
+
   mongoose
-    .connect(address)
+    .connect(address, { autoIndex })
     // eslint-disable-next-line
     .then(() => console.log("Connection to database is established"))
     // eslint-disable-next-line
